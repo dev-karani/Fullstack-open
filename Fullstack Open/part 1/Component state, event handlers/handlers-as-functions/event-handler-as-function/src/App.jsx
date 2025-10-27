41504f0530eac9e2f00d1279c033bed179fcb81e
dev-karani <edwin.k.kithinji@gmail.com>
2025-10-27 16:26:39 +0300

    handlers as function

--- Fullstack Open/part 1/Component state, event handlers/handlers-as-functions/event-handler-as-function/src/App.jsx
+++ Fullstack Open/part 1/Component state, event handlers/handlers-as-functions/event-handler-as-function/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
-//in this lesson, the teacher emphasizes 
+//in this lesson, the teacher emphasizes making handlers as functions 
+//eg if u pass setcounter as a function call.it will result to a loop
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
