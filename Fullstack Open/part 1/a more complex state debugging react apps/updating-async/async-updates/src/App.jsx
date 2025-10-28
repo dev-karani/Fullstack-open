84cd2710b78fe8ee2ff3a346eb094fdb46be5019
dev-karani <edwin.k.kithinji@gmail.com>
2025-10-28 15:27:54 +0300

    feat:added debugging aync updates

---
+++ Fullstack Open/part 1/a more complex state debugging react apps/updating-async/async-updates/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+
+
+//from this we see the button updates the total but tis less behind.
+//this is as it is aync, not immeadiately , but at some point
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+
+//   const [total, setTotal] = useState(0)
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     console.log('left before', left)
+//     setLeft(left + 1)
+//     console.log('left after', left)
+//     setTotal(left + right) 
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     setRight(right + 1)
+
+//     setTotal(left + right)
+//   }
+
+//   return (
+//     <div>
+//       {left}
+//       <button onClick={handleLeftClick}>left</button>
+//       <button onClick={handleRightClick}>right</button>
+//       {right}
+//       <p>{allClicks.join(' ')}</p>
+
+//       <p>total {total}</p>
+//     </div>
+//   )
+// }
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
+    </div>
+  )
+}
+export default App
