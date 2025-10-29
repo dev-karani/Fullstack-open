86d83af9194bb95d3ddfa73fae6dacefdedfd0d1
dev-karani <edwin.k.kithinji@gmail.com>
2025-10-29 11:39:00 +0300

    update:ordered the folders

---
+++ Fullstack Open/part 1/4. a more complex state debugging react apps/5. function-to-function/function-to-function/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+
+//this lesson a different way to cover functions
+//when passing event handlers one can set them inside another function 
+//then return taht value
+//and due to single statements it can be refactored further
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      {value}
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
+    </div>
+  )
+}
+
+export default App
