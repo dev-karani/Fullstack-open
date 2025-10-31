d87445a9174026eff21c1920fb7b250f555d6623
dev-karani <edwin.k.kithinji@gmail.com>
2025-10-31 16:01:17 +0300

    added: celsius exercise

--- Fullstack Open/part 1/extra-side-exercises/counter with history/celsius-fahrenheit/celsius-fahrneheit/src/App.jsx
+++ Fullstack Open/part 1/extra-side-exercises/counter with history/celsius-fahrenheit/celsius-fahrneheit/src/App.jsx
@@ -1,34 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [celsius, setCelsius] = useState(0)
+  const [fahrenheit, setFahrenheit] = useState(32)
+
+  // when Celsius changes
+  const handleCelsiusChange = (event) => {
+    const c = event.target.value
+    setCelsius(c)
+    setFahrenheit((c * 9/5) + 32)
+  }
+
+  // when Fahrenheit changes
+  const handleFahrenheitChange = (event) => {
+    const f = event.target.value
+    setFahrenheit(f)
+    setCelsius((f - 32) * 5/9)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ margin: '2rem' }}>
+      <h2>Temperature Converter</h2>
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className="form-example">
+          <label htmlFor="fahrenheit">Fahrenheit: </label>
+          <input
+            type="number"
+            id="fahrenheit"
+            value={fahrenheit}
+            onChange={handleFahrenheitChange}
+          />
+        </div>
+
+        <div className="form-example">
+          <label htmlFor="celsius">Celsius: </label>
+          <input
+            type="number"
+            id="celsius"
+            value={celsius}
+            onChange={handleCelsiusChange}
+          />
+        </div>
+      </form>
+    </div>
   )
 }
 
