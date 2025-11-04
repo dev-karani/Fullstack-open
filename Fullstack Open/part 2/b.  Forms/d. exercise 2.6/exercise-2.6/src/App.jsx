ee6a6a1264474b1330148e8603353635734322c0
dev-karani <edwin.k.kithinji@gmail.com>
2025-11-04 17:41:50 +0300

    feat:add phonebook exercise

---
+++ Fullstack Open/part 2/b.  Forms/d. exercise 2.6/exercise-2.6/src/App.jsx
@@ -0,0 +1,45 @@
+  import { useState } from 'react'
+
+  //create phonebook, add member and have it display
+const App =() =>{
+  const [person, setNewPerson] = useState([
+    {name: 'arto hellas'}
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event)=> {
+    event.preventDefault()
+    const personObject = {
+      name:newName}
+    setNewPerson(person.concat(personObject))
+    setNewName('')
+  }
+  // console.log(person)
+  const handlePersonChange = (event) => setNewName(event.target.value)
+
+  
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form 
+      onSubmit={addPerson}
+      >
+        <div>
+          name: <input  onChange={handlePersonChange} value={newName}/>
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {person.map((person, i) => (
+        <p key={i}>{person.name}</p>
+      ))}
+      {/* <div>debug: {newName}</div> */}
+    </div>
+  )
+}
+
+
+export default App
