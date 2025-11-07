00792d8bd79a76b15c4e8cc052ed7efa116bad01
dev-karani <edwin.k.kithinji@gmail.com>
2025-11-07 17:45:23 +0300

    feat:practiced express routes and middleware

--- Fullstack Open/part 3/a. Node.js and Express/simple web server/index.js
+++ Fullstack Open/part 3/a. Node.js and Express/simple web server/index.js
@@ -22,10 +22,31 @@ let notes = [
   }
 ]
 
-app.post('/api/notes', (reques, response)=>{
-  const note = request.body
-  console.log(note);
-  response.json
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 app.get('/', (request, response) => {
