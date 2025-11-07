1265c8a3e9bbf5d2ba15f9a236e6623bb51eeae1
dev-karani <edwin.k.kithinji@gmail.com>
2025-11-07 09:01:25 +0300

    update:styling react apps

---
+++ Fullstack Open/part 2/e.  Adding styles to a react app/improved error messages/styles/src/components/notification.jsx
@@ -0,0 +1,12 @@
+const Notification = ({message}) =>{
+    if (message ===null){
+        return nul
+    }
+    return(
+        <div className='error' > 
+            {message}
+        </div>
+    )
+}
+
+export default Notification
